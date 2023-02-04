b7e8df4f96429786aab931d3553d2b0899cb8396
VIPIN K P <58673683+VipinVIP@users.noreply.github.com>
2023-02-04 19:16:35 +0530

    📝 Shorten and document First and follow code

--- experiments/first and follow/README.md
+++ experiments/first and follow/README.md
@@ -1,3 +1,45 @@
-# First and follow
+# First and Follow
+Write a program to find First and Follow of any given grammar.
+
 ## Algorithm
+1. Get the number of productions in the grammar.
+2. Read the productions and store them in an array.
+3. Get the element whose First and Follow need to be found.
+4. Call the "first" function passing the element as argument.<br>
+    1. If its a terminal, add itself to the first array.<br>
+    2. Check if the Production rule start with passed element. If yes<br>
+        1. check if the first element after '=' is an uppercase(Non-terminal) or lowercase (terminal).<br>
+        2. If lower, add that to the first array.<br>
+        3. If upper, call first() on that element.<br>
+5. Call the "follow" function passing the element as argument.<br>
+    1. if its Starting production, add $ to follow array.<br>
+    2. For each production, check if the symbol appears in the production.If yes:<br>
+        1. If its not the last symbol in production, calculate the First of the symbol<br>
+        2. if it's the last, calculate the Follow of the first symbol of that production.
+6. Print the First and Follow of the element.
+
 ## Output
+
+```
+❯ ./a.out
+
+No of prooductions : 3
+
+Enter the productions :
+S=aAbc
+A=c
+A=d
+
+Element whose first & follow is to be found : S
+First(S)={a }
+Follow(S)={$ }
+
+Continue(0/1) : 1
+
+Element whose first & follow is to be found : A
+First(A)={c d }
+Follow(A)={b }
+
+Continue(0/1) : 0
+
+```
--- experiments/first and follow/firstandfollow.c
+++ experiments/first and follow/firstandfollow.c
@@ -1,67 +1,75 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
-int n, m = 0, p, i = 0, j = 0;
-char a[10][10], f[10];
+#include <string.h>
+
+int n, m , p, i , j ;
+char production[10][10], f[10];
 void follow(char c);
 void first(char c);
-int main(){
-    int i, z;
-    char c, ch;
-    printf("No of prooductions:");
-    scanf("%d", &n);
-    printf("Enter the productions:\n");
-    for (i = 0; i < n; i++)
-        scanf("%s%c", a[i], &ch);
-    do{
-        m = 0;
-        printf("Elemet whose fisrt & follow is to be found:");
-        scanf("%c", &c);
-        first(c);
-        printf("First(%c)={", c);
-        for (i = 0; i < m; i++)
-            printf("%c", f[i]);
-        printf("}\n");
-        strcpy(f, " ");
-        m = 0;
-        follow(c);
-        printf("Follow(%c)={", c);
-        for (i = 0; i < m; i++)
-            printf("%c", f[i]);
-        printf("}\n");
-        printf("Continue(0/1)?");
-        scanf("%d%c", &z, &ch);
-    } while (z == 1);
-    return (0);
+
+int main() {
+  int z;
+  char c;
+  printf("\nNo of prooductions : ");
+  scanf("%d", &n);
+  printf("\nEnter the productions :\n");
+  for (i = 0; i < n; i++) {
+    scanf("%s", production[i]);
+    getchar(); // to remove error caused by enter key
+  }
+  do {
+    printf("\nElement whose first & follow is to be found : ");
+    scanf("%c", &c);
+
+    m = 0;
+    first(c);
+    printf("First(%c)={", c);
+    for (i = 0; i < m; i++)
+      printf("%c ", f[i]);
+    printf("}\n");
+    strcpy(f, " ");
+
+    m = 0;
+    follow(c);
+    printf("Follow(%c)={", c);
+    for (i = 0; i < m; i++)
+      printf("%c ", f[i]);
+    printf("}\n");
+
+    printf("\nContinue(0/1) : ");
+    scanf("%d", &z);
+    getchar(); // to remove error caused by enter key
+  } while (z == 1);
+
+  return (0);
 }
-void first(char c){
-    int k;
-    if (!isupper(c))
-        f[m++] = c;
-    for (k = 0; k < n; k++) {
-        if (a[k][0] == c){
-            if (a[k][2] == '$')
-                follow(a[k][0]);
-            else if (islower(a[k][2]))
-                f[m++] = a[k][2];
-            else
-                first(a[k][2]);
-        }
+
+void first(char c) {
+  int k;
+  if (!isupper(c))
+    f[m++] = c;
+  for (k = 0; k < n; k++) {
+    if (production[k][0] == c) {
+       if (islower(production[k][2]))
+        f[m++] = production[k][2];
+      else
+        first(production[k][2]);
     }
+  }
 }
-void follow(char c){
-    if (a[0][0] == c)
-        f[m++] = '$';
-    for (i = 0; i < n; i++){
-        for (j = 2; j < strlen(a[i]); j++){
-            if (a[i][j] == c){
-                if (a[i][j + 1] != '\0')
-                    first(a[i][j + 1]);
-                if (a[i][j + 1] == '\0' && c != a[i][0])
-                    follow(a[i][0]);
-            }
-        }
+
+void follow(char c) {
+  if (production[0][0] == c)
+    f[m++] = '$';
+  for (i = 0; i < n; i++) {
+    for (j = 2; j < strlen(production[i]); j++) {
+      if (production[i][j] == c) {
+        if (production[i][j + 1] != '\0')
+          first(production[i][j + 1]);
+        if (production[i][j + 1] == '\0' && c != production[i][0])
+          follow(production[i][0]);
+      }
     }
+  }
 }
