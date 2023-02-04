818cd21254a343c612d3d17ae1a7efb24b0c4577
VIPIN K P <58673683+VipinVIP@users.noreply.github.com>
2023-02-04 18:38:36 +0530

    📝 Shorten and document Shift reducer code

--- experiments/shift reduce parser/README.md
+++ experiments/shift reduce parser/README.md
@@ -1,3 +1,50 @@
 # Shift reduce parser
+Construct a Shift Reduce Parser for a given language
+
 ## Algorithm
+
+1. Initialize variables:<br>
+    a. buffer: to store the input string<br>
+    b. stk: to store the intermediate results<br>
+    c. len: to store the length of the input string<br>
+2. Input the string to be parsed and store it in the buffer
+3. Set the length of the input string to the variable "len"
+4. For each character in the buffer, check if it and the next character form "id".
+5. If the next 2 characters are "id", shift and store both characters into the stack.
+6. If the characters are not "id", shift the character into the stack.
+7. Call the check function to reduce the stack according to the grammar.
+8. In the check function, check for the patterns 'id', 'E + E', 'E * E', and '(E)'.
+9. If any of the patterns is found, replace it with 'E' in the stack and update the stack.
+10. Repeat steps 7-9 until the stack contains only one element and input becomes empty.
+
 ## Output
+
+```
+❯ ./a.out
+
+GRAMMAR is 
+ E->E+E 
+ E->E*E 
+ E->(E) 
+ E->id 
+
+Enter input string : id+id*id+id
+
+stack    input   action
+
+$id       +id*id+id$    SHIFT->id
+$E        +id*id+id$    REDUCE TO E
+$E+        id*id+id$    SHIFT->symbol +
+$E+id        *id+id$    SHIFT->id
+$E+E         *id+id$    REDUCE TO E
+$E           *id+id$    REDUCE TO E
+$E*           id+id$    SHIFT->symbol *
+$E*id           +id$    SHIFT->id
+$E*E            +id$    REDUCE TO E
+$E              +id$    REDUCE TO E
+$E+              id$    SHIFT->symbol +
+$E+id              $    SHIFT->id
+$E+E               $    REDUCE TO E
+$E                 $    REDUCE TO E
+
+```
--- experiments/shift reduce parser/shiftreduce.c
+++ experiments/shift reduce parser/shiftreduce.c
@@ -1,75 +1,64 @@
-//E8 Shift reduce parser
 #include <stdio.h>
 #include <string.h>
-int k = 0, z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[15], act[10];
+int z = 0, i = 0, j = 0, len = 0;
+char buffer[16], ac[20], stk[15], act[10];
 void check();
-int main()
-{
-  puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id");
-  puts("enter input string ");
-  scanf("%s", a);
-  c = strlen(a);
-  strcpy(act, "SHIFT->");
-  puts("stack \t input \t action");
-  for (k = 0, i = 0; j < c; k++, i++, j++)
-  {
-    if (a[j] == 'i' && a[j + 1] == 'd')
-    {
-      stk[i] = a[j];
-      stk[i + 1] = a[j + 1];
+
+int main() {
+  printf("\nGRAMMAR is \n E->E+E \n E->E*E \n E->(E) \n E->id \n");
+  printf("\nEnter input string : ");
+  scanf("%s", buffer);
+  len = strlen(buffer);
+  printf("stack \t input \t action\n");
+  for (i = 0; j < len; i++, j++) {
+    if (buffer[j] == 'i' && buffer[j + 1] == 'd') {
+      stk[i] = buffer[j];
+      stk[i + 1] = buffer[j + 1];
       stk[i + 2] = '\0';
-      a[j] = ' ';
-      a[j + 1] = ' ';
-      printf("\n$%s\t%s$\t%sid", stk, a, act);
+      buffer[j] = ' ';
+      buffer[j + 1] = ' ';
+      printf("\n$%s\t%s$\tSHIFT->id", stk, buffer);
       check();
-    }
-    else
-    {
-      stk[i] = a[j];
+    } else {
+      stk[i] = buffer[j];
       stk[i + 1] = '\0';
-      a[j] = ' ';
-      printf("\n$%s\t%s$\t%ssymbols", stk, a, act);
+      buffer[j] = ' ';
+      printf("\n$%s\t%s$\tSHIFT->symbol %c", stk, buffer,stk[i]);
       check();
     }
   }
 }
-void check()
-{
-  strcpy(ac, "REDUCE TO E");
-  for (z = 0; z < c; z++)
-    if (stk[z] == 'i' && stk[z + 1] == 'd')
-    {
+void check() {
+
+  for (z = 0; z < len; z++)
+    if (stk[z] == 'i' && stk[z + 1] == 'd') {
       stk[z] = 'E';
       stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
+      printf("\n$%s\t%s$\tREDUCE TO E", stk, buffer);
       j++;
     }
-  for (z = 0; z < c; z++)
-    if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E')
-    {
+
+  for (z = 0; z < len; z++)
+    if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') {
       stk[z] = 'E';
       stk[z + 1] = '\0';
-      stk[z + 2] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
+      printf("\n$%s\t%s$\tREDUCE TO E", stk, buffer);
       i = i - 2;
     }
-  for (z = 0; z < c; z++)
-    if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E')
-    {
+
+  for (z = 0; z < len; z++)
+    if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') {
       stk[z] = 'E';
       stk[z + 1] = '\0';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
+      printf("\n$%s\t%s$\tREDUCE TO E", stk, buffer);
       i = i - 2;
     }
-  for (z = 0; z < c; z++)
-    if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
-    {
+    
+  for (z = 0; z < len; z++)
+    if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')') {
       stk[z] = 'E';
       stk[z + 1] = '\0';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
+      printf("\n$%s\t%s$\tREDUCE TO E", stk, buffer);
       i = i - 2;
     }
 }
